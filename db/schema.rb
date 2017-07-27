720c0395533fc550580536980594759a11dcd099
andrew <danteadar@gmail.com>
2017-07-26 20:54:17 -0700

    Added attributes to the comments connection_requests events and posts models

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721032852) do
+ActiveRecord::Schema.define(version: 20170727034752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "commenter_id"
+    t.string   "commentable_type"
+    t.integer  "commentable_id"
+    t.text     "body"
   end
 
   create_table "connection_requests", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.string   "status"
   end
 
   create_table "events", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "host_id"
+    t.string   "title"
+    t.string   "photo"
+    t.datetime "date"
+    t.string   "ticket_link"
+    t.text     "about"
   end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
   end
 
   create_table "users", force: :cascade do |t|
