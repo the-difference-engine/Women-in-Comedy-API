199a92af58a72e68e154e94e76a0451e9a37f551
Ken Lu <kmlu1993@gmail.com>
2017-08-07 20:41:58 -0700

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://qa-womenincomedy.herokuapp.com/' }
   Rails.application.routes.default_url_options[:host] = 'https://qa-womenincomedy.herokuapp.com/'
+
+
 end
