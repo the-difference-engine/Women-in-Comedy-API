2db332e987cb63d7a649c2f7ab10007ad6c53180
Ken Lu <kmlu1993@gmail.com>
2017-08-09 14:07:03 -0700

    email

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -17,7 +17,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.sendgrid.net',
   port:                 587,
-  domain:               'heroku.com',
+  domain:               'https://qa-womenincomedy.herokuapp.com/',
   user_name:            ENV["SENDGRID_USERNAME"],
   password:             ENV["SENDGRID_PASSWORD"],
   authentication:       'plain',
