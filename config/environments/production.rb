51d9d332a0d6354c9ca2402bf15233773e194c2b
Ken Lu <kmlu1993@gmail.com>
2017-08-11 23:31:38 -0700

    fix please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://qa-womenincomedy.herokuapp.com/'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 25,
+    address:              'smtp.sendgrid.net',
+    port:                 587,
     domain:               'https://qa-womenincomedy.herokuapp.com/',
     user_name:            'testwomenincomedy@gmail.com',
     password:             'womenincomedy',
