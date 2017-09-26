53545716f428cbe6f5238565af70521bbf6a9f08
Ken Lu <kmlu1993@gmail.com>
2017-09-25 19:38:54 -0700

    added admin to user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918221355) do
+ActiveRecord::Schema.define(version: 20170926023542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,7 +97,6 @@ ActiveRecord::Schema.define(version: 20170918221355) do
     t.text     "meeting"
     t.string   "birthdate"
     t.boolean  "admin"
-    t.boolean  "super_user"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
