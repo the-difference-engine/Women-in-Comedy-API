1af47540f4ade6deb1a973251158e9f38f1a691f
Daniel Mai <danielmai1728@gmail.com>
2017-11-21 21:16:22 -0800

    update session controller to include admin

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     user = User.where(email: params[:email]).first
     if user && user.valid_password?(params[:password])
       User.current_user = user
-      render json: user.as_json(only: [:id, :email, :confirmed_at])
+      render json: user.as_json(only: [:id, :admin, :email, :confirmed_at])
     else
       head(:unauthorized)
     end
