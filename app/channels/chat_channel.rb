1ba02b8c3e45dfb7967383fe3649953eb08d7b05
Daniel Mai <danielmai1728@gmail.com>
2017-12-07 20:47:54 -0800

    add create method in chat_channel

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,8 +1,14 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "chat_channel"
+    stream_from 'chat_channel'
   end
 
   def unsubscribed
   end
+
+  def create(options)
+   ChatMessage.create(
+     content: options.fetch('content')
+   )
+ end
 end
