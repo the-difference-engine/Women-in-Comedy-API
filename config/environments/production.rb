ca664f65b3030acefa56087e1695781dd6d191c2
Daniel Mai <danielmai1728@gmail.com>
2017-12-12 21:16:49 -0800

    fix localhost origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://qa-womenincomedy.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://localhost:3000/', /http:\/\/example.*/ ]
+  # config.action_cable.allowed_request_origins = [ 'http://localhost:3000/', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['http://localhost:3000']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
