d3e89e523ae8f46f4885efc72da443374c10adea
Daniel Mai <danielmai1728@gmail.com>
2017-12-14 21:08:53 -0800

    update to reflect change in schema and Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171108235123) do
-
+ActiveRecord::Schema.define(version: 20171210022419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
