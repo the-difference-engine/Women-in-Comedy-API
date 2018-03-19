69ced22a494d81558f20331c6a4f7e1155f299db
Manita Herlitz-Ferguson <manita.herlitzferguson@gmail.com>
2018-03-19 18:32:30 -0500

    migration to default admin status for users to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312234640) do
+ActiveRecord::Schema.define(version: 20180319232239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,12 +96,12 @@ ActiveRecord::Schema.define(version: 20180312234640) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                     default: "", null: false
-    t.string   "encrypted_password",        default: "", null: false
+    t.string   "email",                     default: "",    null: false
+    t.string   "encrypted_password",        default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",             default: 0,  null: false
+    t.integer  "sign_in_count",             default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -110,8 +110,8 @@ ActiveRecord::Schema.define(version: 20180312234640) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "first_name"
@@ -125,7 +125,7 @@ ActiveRecord::Schema.define(version: 20180312234640) do
     t.string   "experience"
     t.text     "meeting"
     t.string   "birthdate"
-    t.boolean  "admin"
+    t.boolean  "admin",                     default: false
     t.string   "photo"
     t.boolean  "block_connection_requests"
     t.datetime "suspended_at"
