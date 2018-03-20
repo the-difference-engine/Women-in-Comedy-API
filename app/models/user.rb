c003965cf04dfdb85d539a5fdc275fae57e71b4e
Manita Herlitz-Ferguson <manita.herlitzferguson@gmail.com>
2018-03-19 20:47:10 -0500

    use oauth and turn confirmation back on

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 
   has_many :events
   has_many :posts, as: :postable
-  # has_and_belongs_to_many :oauth_credentials
+  has_and_belongs_to_many :oauth_credentials
   has_many :notifications, as: :recipient
 
   def self.from_omniauth(auth)
@@ -32,7 +32,7 @@ class User < ApplicationRecord
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
-      user.skip_confirmation!
+      # user.skip_confirmation!
     end
   end
 
