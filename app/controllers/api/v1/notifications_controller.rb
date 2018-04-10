13f43cc1bb55f74e4be9cded4a68669fca213e96
Caroline <carolinegferris@gmail.com>
2018-04-09 19:56:17 -0500

    add mark_one_as_read action for post request to update only one notification as seen

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::NotificationsController < ApplicationController
   # before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
 
   def index
     @notifications = Notification.where(recipient: current_user).recent
   end
 
   def mark_all_as_read
-    Rails.logger.info "Inside mark_all_as_read"
+    # Rails.logger.info "Inside mark_all_as_read"
     Notification.where(recipient_id: params[:id], seen: nil).update_all(seen: Time.zone.now)
 
     @notifications = Notification.where(recipient_id: params[:id])
     render 'get_notifications.json.jbuilder'
   end
 
-  def mark_as_read
-    # byebug
-    Notification.where(id: params[:notification_id], recipient_id: params[:id], seen: nil).update(seen: Time.zone.now)
-
-    @notifications = Notification.where(recipient_id: params[:id])
+  def mark_one_as_read
+    p params[:notification]
+    Notification.where(recipient_id: params[:user_id], id: params[:id], seen: nil).update_all(seen: Time.zone.now)
+    @notifications = Notification.where(recipient_id: params[:user_id])
 
     render 'get_notifications.json.jbuilder'
   end
