7cba5bbe243e78869aab1a3854728a9bcd290b2f
Manita Herlitz-Ferguson <manita.herlitzferguson@gmail.com>
2018-04-30 20:04:13 -0500

    update after_create for user blocks

--- app/models/user_block.rb
+++ app/models/user_block.rb
@@ -4,14 +4,14 @@ class UserBlock < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   after_create do
-    if ConnectionRequest.exists?(sender_id: params[:sender_id], receiver_id:params[:receiver_id])
-      connection_request = ConnectionRequest.find(sender_id: params[:sender_id], receiver_id:params[:receiver_id])
+    connection_request = ConnectionRequest.where(sender_id: params[:sender_id], receiver_id:params[:receiver_id]).first
+    if connection_request.present?
       connection_request.destroy
     end
-    if ConnectionRequest.exists?(sender_id: params[:receiver_id], receiver_id: params[:sender_id])
-      connection_request = ConnectionRequest.find(sender_id: params[:receiver_id], receiver_id:params[:sender_id])
+  else
+    connection_request = ConnectionRequest.where(sender_id: params[:receiver_id], receiver_id:params[:sender_id]).first
+    if connection_request.present?
       connection_request.destroy
     end
   end
-  
 end
