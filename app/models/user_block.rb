0e0a209cef410a809d0948095b103bc4b5958bd6
Manita Herlitz-Ferguson <manita.herlitzferguson@gmail.com>
2018-05-08 22:45:36 -0500

    fix after_create hook

--- app/models/user_block.rb
+++ app/models/user_block.rb
@@ -4,12 +4,12 @@ class UserBlock < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   after_create do
-    connection_request = ConnectionRequest.where(sender_id: params[:sender_id], receiver_id:params[:receiver_id]).first
+    connection_request = ConnectionRequest.where(sender_id: self.sender_id, receiver_id: self.receiver_id).first
     if connection_request.present?
       connection_request.destroy
     end
   else
-    connection_request = ConnectionRequest.where(sender_id: params[:receiver_id], receiver_id:params[:sender_id]).first
+    connection_request = ConnectionRequest.where(sender_id: self.receiver_id, receiver_id: self.sender_id).first
     if connection_request.present?
       connection_request.destroy
     end
