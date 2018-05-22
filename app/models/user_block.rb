35796000af86fd46f0a812cdcaa85244c36e4666
Marius Maries <mariuscm@gmail.com>
2018-05-21 23:41:27 -0500

    modify user block creation in the seeds

--- app/models/user_block.rb
+++ app/models/user_block.rb
@@ -1,17 +1,25 @@
 class UserBlock < ApplicationRecord
 
-  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
-  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :blocker, class_name: 'User', foreign_key: 'blocker_id'
+  belongs_to :blocked, class_name: 'User', foreign_key: 'blocked_id'
+  validate :unique_record
 
   after_create do
-    connection_request = ConnectionRequest.where(sender_id: self.sender_id, receiver_id: self.receiver_id).first
+    connection_request = ConnectionRequest.where(sender_id: self.blocker_id, receiver_id: self.blocked_id).first
     if connection_request.present?
       connection_request.destroy
     end
   else
-    connection_request = ConnectionRequest.where(sender_id: self.receiver_id, receiver_id: self.sender_id).first
+    connection_request = ConnectionRequest.where(sender_id: self.blocked_id, receiver_id: self.blocker_id).first
     if connection_request.present?
       connection_request.destroy
     end
+  end
+
+  private
+  def unique_record
+    if UserBlock.exists?(blocker_id: self.blocker_id, blocked_id: self.blocked_id) || UserBlock.exists?(blocker_id: self.blocked_id, blocked_id: self.blocker_id)
+      return false
+    end 
   end
 end
