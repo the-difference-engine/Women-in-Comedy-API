24a8b18e7e556196ef0b65f1b6bbe46dda74ad4e
Marius Maries <mariuscm@gmail.com>
2018-06-16 15:21:23 -0500

    refactor blocks into blockers and blocked_bys; fix destroy method

--- app/controllers/api/v1/user_blocks_controller.rb
+++ app/controllers/api/v1/user_blocks_controller.rb
@@ -1,18 +1,26 @@
 class Api::V1::UserBlocksController < ApplicationController
   skip_before_action :verify_authenticity_token
+  
   def get_blocked_users
     id = request.headers['id'].to_i
     #query block request where blocker_id is equal to user Id and blocked_id is equal to User id.
-    blocks = UserBlock.where(blocker_id: id, blocked_id: id).uniq
+    blockers = UserBlock.where(blocker_id: id)
+    blocked_bys = UserBlock.where(blocked_id: id)
+    
     #declare an array for all the blocked users
     user_array = []
-    #each through the blocks and push the user to the user_array
-    blocks.each do |block|
+    #each through the blockers and push the user to the user_array
+    blockers.each do |block|
         user = User.find_by(id: block[:blocked_id])
         user = {id: user[:id], firstName: user[:first_name], lastName: user[:last_name]}
         user_array.push(user)
     end
-    render json: user_array
+    blocked_bys.each do |block|
+      user = User.find_by(id: block[:blocker_id])
+      user = {id: user[:id], firstName: user[:first_name], lastName: user[:last_name]}
+      user_array.push(user)
+    end
+    render json: user_array.uniq
   end
   def get_blocked_by
     id = request.headers['id'].to_i
@@ -29,14 +37,14 @@ class Api::V1::UserBlocksController < ApplicationController
     render json: user_array
   end
   def create
-    block_request = UserBlock.create(
+    block = UserBlock.create(
       blocker_id: params[:blocker_id],
       blocked_id: params[:blocked_id]
     )
-    render json: block_request.as_json
+    render json: block.as_json
   end
   def destroy
-    block_request = UserBlock.find_by(id: params[:id])
-    block_request.destroy
+    block = UserBlock.find_by(id: params[:id])
+    block.destroy
   end
 end
