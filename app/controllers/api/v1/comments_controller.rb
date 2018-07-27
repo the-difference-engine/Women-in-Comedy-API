47f0e2be66ea1a36b8e06f7a604863a1ed6046e2
Audrey <anfletcher24@gmail.com>
2018-07-26 20:28:03 -0500

    adjusted styling on posts

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::CommentsController < ApplicationController
+    protect_from_forgery prepend: true
+	def index
+		@comments = Comment.all
+
+		render 'index.json.jbuilder'
+	end
+
+    def create
+		comment = Comment.create(
+			body: params[:body],
+            author_id: params[:authorId]
+            post_id: params[:postId]
+		)
+		render json: comment
+	end
+
 end
+
+
+   
