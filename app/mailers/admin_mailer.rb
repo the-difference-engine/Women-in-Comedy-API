d6fed0f3aca2d6d911fe682862f922e6e9aeb6e3
Michael Kirlin <michael.kirlin@here.com>
2018-09-28 09:26:02 -0500

    Linting mailer files

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,11 +3,7 @@ class AdminMailer < ActionMailer::Base
 
   default template_path: 'admin_mailer'
   def email_all_users(email, subject)
-    
     @users = User.all.collect(&:email)
-    
     mail(to: @users, subject: subject, body: email, content_type: 'text/html')
-    
   end
-
 end
