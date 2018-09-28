5fcc364cb1ac3b34abea2a0bbbd64895513e9cb7
Michael Kirlin <michael.kirlin@here.com>
2018-09-28 09:29:43 -0500

    Linting job files

--- app/jobs/notification_relay_job.rb
+++ app/jobs/notification_relay_job.rb
@@ -2,7 +2,15 @@ class NotificationRelayJob < ApplicationJob
   queue_as :default
 
   def perform(notification)
-    html = ApplicationController.render partial: "notifications/#{notification.notifiable_type.underscore.pluralize}/#{notification.action}", locals: {notification: notification}, formats: [:html]
-    ActionCable.server.broadcast "notifications:#{notification.recipient_id}", html: html
+    html = ApplicationController.render(
+      partial: "notifications/#{notification.notifiable_type.underscore.pluralize}/#{notification.action}",
+      locals: { notification: notification },
+      formats: [:html]
+    )
+
+    ActionCable.server.broadcast(
+      "notifications:#{notification.recipient_id}",
+      html: html
+    )
   end
 end
