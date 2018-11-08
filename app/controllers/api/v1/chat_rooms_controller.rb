f46e3e4b01a837453311754b8f12f66c0fbc6ab2
Bryan Vanderhoof <bvanderhoof@heroku.com>
2018-11-07 19:27:44 -0600

    Fix stray question mark

--- app/controllers/api/v1/chat_rooms_controller.rb
+++ app/controllers/api/v1/chat_rooms_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::ChatRoomsController < ApplicationController
 
   def create
     target = User.find_by(id: params[:target_user])
-    head(:conflict) unless target?
+    head(:conflict) unless target
 
     # try to find an existing chat room with these two users
     room = current_user.chat_rooms.where(:private: true).includes(:users).where("users.id" => target.id).includes(:chat_messages).first
