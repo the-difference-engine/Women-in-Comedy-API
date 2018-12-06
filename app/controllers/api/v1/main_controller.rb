74888836eff2c1120ff87d823300329fb54fb14e
DeAndre Boston <dboston3@yahoo.com>
2018-12-05 18:05:14 -0600

    New search bar

---
+++ app/controllers/api/v1/main_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::MainController < ApplicationController
+  before_action :force_json, only: :search
+
+  def index; end
+
+  def search
+    users = User.ransack(first_name_or_last_name_or_email_cont: params[:q]).result(distinct: true).limit(10)
+    render json: users.as_json(only: [:q, :first_name, :last_name])
+  end
+
+  private
+
+  def force_json
+    request.format = :json
+  end
+end
--- app/views/main/search.json.jbuilder
+++ app/views/main/search.json.jbuilder
@@ -2,5 +2,7 @@ json.users do
   json.array! (@users) do |user|
     json.first_name user.first_name
     json.last_name user.last_name
+    json.id user.id
+    json.email user.email
   end
 end
